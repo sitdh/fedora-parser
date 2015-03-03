d3a2cd247cee208d8d976cd8f3d711fee7d04151
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-03-03 20:44:08 +0700

    Add Root element

--- fedora.py
+++ fedora.py
@@ -3,7 +3,15 @@ from fedora.manager.manager import FedoraConnectionManager
 
 if '__main__' == __name__:
     fedoraTemplate = FedoraTemplate()
-    parser = FedoraConnectionManager("http://localhost:8080/rest/hand/english/fcr:metadata", templates=[FedoraTemplate()]);
-    data = parser.retrieve_information()
+
+    endpoint = 'http://localhost:8080/rest'
+    # endpoint = 'http://localhost:8080/rest/fcr:metadata'
+    # endpoint = 'http://localhost:8080/rest/hand/english/fcr:metadata'
+    parser = FedoraConnectionManager(
+            endpoint,
+            templates=[FedoraTemplate()]
+        );
+    # data = parser.retrieve_information()
+    data = parser.retrieve_information(root_node=True)
 
     print data 

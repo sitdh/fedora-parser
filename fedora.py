a25bf3c661f69903508bd719723cca084c793058
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-03-04 18:20:20 +0700

    change parser methods

--- fedora.py
+++ fedora.py
@@ -1,17 +1,22 @@
 from fedora.template.fedora import FedoraTemplate
+from fedora.template.root import FedoraRootNodeTemplate 
 from fedora.manager.manager import FedoraConnectionManager
 
 if '__main__' == __name__:
     fedoraTemplate = FedoraTemplate()
 
-    endpoint = 'http://localhost:8080/rest'
+    # endpoint = 'http://localhost:8080/rest'
     # endpoint = 'http://localhost:8080/rest/fcr:metadata'
-    # endpoint = 'http://localhost:8080/rest/hand/english/fcr:metadata'
+    endpoint = 'http://localhost:8080/rest/hand/english/fcr:metadata'
+
+    # template = FedoraRootNodeTemplate()
+    template = FedoraTemplate()
+
     parser = FedoraConnectionManager(
             endpoint,
-            templates=[FedoraTemplate()]
+            templates=[template]
         );
     # data = parser.retrieve_information()
-    data = parser.retrieve_information(root_node=True)
+    data = parser.retrieve_information()
 
     print data 
